ed97d61fa9324fb6adee3b9fc10d502a87521e25
YEONGORI <dusrjf14@gmail.com>
2023-11-02 16:05:26 +0900

    [init] init project

---
+++ src/main/java/sanhak/shserver/ShServerApplication.java
@@ -0,0 +1,13 @@
+package sanhak.shserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sanhak/shserver/ShServerApplicationTests.java
@@ -0,0 +1,13 @@
+package sanhak.shserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
